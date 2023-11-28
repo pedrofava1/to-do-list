98c5a3c54b559dba57e624956ecb3de59e1f8d89
pedrofava1 <pedro.fava@unifesp.br>
2023-11-28 00:29:38 -0300

    Remove dist/models do rastreamento do Git

--- dist/models/TaskStatus.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TaskStatus = void 0;
-var TaskStatus;
-(function (TaskStatus) {
-    TaskStatus["TODO"] = "TODO";
-    TaskStatus["COMPLETED"] = "COMPLETED";
-})(TaskStatus || (exports.TaskStatus = TaskStatus = {}));
